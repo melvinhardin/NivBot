eac889b34bba8efae437100477d812218ee136ad
melvinhardin <melvinhardin14071991@gmail.com>
2026-07-20 07:57:08 +0200

    Made the base requirements for discord connection

--- NivBot/Program.cs
+++ NivBot/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello World");
+﻿using Microsoft.Extensions.Configuration;
+using NetCord;
+using NetCord.Gateway;
+using NetCord.Logging;
+using System.ComponentModel;
+IConfigurationRoot config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddEnvironmentVariables()
+    .Build();
+string discKey = config.GetRequiredSection("Discord").GetRequiredSection("Token").Get<string>();
+
+GatewayClient client = new(new BotToken(discKey), new GatewayClientConfiguration
+{
+    Logger = new ConsoleLogger(),
+});
+
+await client.StartAsync();
+await Task.Delay(-1);

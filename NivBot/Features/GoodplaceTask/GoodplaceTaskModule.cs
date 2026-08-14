22bf84ee7404873264ee6572366d1fc1ed9acbd2
melvinhardin <melvinhardin14071991@gmail.com>
2026-08-14 10:41:22 +0200

    Added scaffolding for the task system

---
+++ NivBot/Features/GoodplaceTask/GoodplaceTaskModule.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NivBot.Features.GoodplaceTask
+{
+    internal class GoodplaceTaskModule
+    {
+    }
+}
---
+++ NivBot/Features/GoodplaceTask/GoodplaceTaskService.cs
@@ -0,0 +1,53 @@
+﻿using NivBot.DataLayer;
+using NivBot.ExternalServicesLayer.OsrsAPI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NivBot.Features.GoodplaceTask
+{
+    public class GoodplaceTaskService(GoodplaceContext db, IOsrsHighscoreService osrsHighscore)
+    {
+        public void GetGoodplaceSkillTask(long discordId)
+        {
+            // See if the discord user exists in the db
+
+            // Collect skill data on the all of the users runescape accounts
+
+            // Check if the user has completed the existing Task and award points
+
+            // Give a task
+                
+            
+
+
+        }
+        public void GetGoodplaceBossTask(long discordId)
+        {
+            // See if the discord user exists in the db
+
+            // Collect activity data on the all of the users runescape accounts
+
+            // Check if the user has completed the existing Task and award points
+
+            // Give a task
+        }
+
+        public void SkipGoodplaceTask(int taskId)
+        {
+            // Check which task
+
+            // Clear the current task
+            
+        }
+        public void BlockGoodplaceTask()
+        {
+            // Check which task
+            
+            // Add the current task to the userTaskBlockList
+            
+            // Skip the current task
+        }
+
+    }
+}
